80e0853815c073cc66e5cd5206d012e8f5fcc847
Greg Russell <greg@metart.com>
2023-01-16 03:26:21 -0800

    testing

--- demo.js
+++ demo.js
@@ -1,7 +1,18 @@
+import Tetris from './lib/tetris.js';
+
 function init() {
-    const canvas = document.querySelector('#board');
+    const canvas = document.getElementById('board');
     const parent = canvas.parentElement;
     const dim = Math.min(parent.clientWidth, parent.clientHeight);
+    const tetris = new Tetris({
+        dim,
+        canvas,
+        debug: true,
+    });
+    tetris.init().then(() => {
+
+    });
+
 }
 
 function onReady() {
